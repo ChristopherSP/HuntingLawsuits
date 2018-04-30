327871b657f922a4cbc9b836f1460c6dbe68a941
ChristopherSP <christopher.unifesp@gmail.com>
2018-04-30 00:16:46 -0300

    Formated

--- scripts/completeProcessAsFunction.R
+++ scripts/completeProcessAsFunction.R
@@ -7,80 +7,100 @@ library(tidytext)
 library(dplyr)
 library(pbmcapply)
 
-args = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly = TRUE)
 
 #################################
 # Check Arguments
 #################################
 if (length(args) != 2) {
-  stop("Error: Two arguments must be provided:\n\t1. PDF diaries folder path\n\t2. Full output txt file path to save the to result", call.=FALSE)
+  stop(
+    "Error: Two arguments must be provided:\n\t1. PDF diaries folder path\n\t2. Full output txt file path to save the to result",
+    call. = FALSE
+  )
 }
 
 inputFolder = args[1]
 outputFile = args[2]
 
 # Split multicolumn text
-structPage = function(page){
-  col1 = stri_extract_all_regex(page,".*(   +)")[[1]]
-  col1 = paste(col1,collapse = '\n')
-  col2 = stri_extract_all_regex(page,"(   +).*\\n")[[1]]
-  col2 = paste(stri_replace_all_regex(col2,"(   +)"," "),collapse = '')
-  text = paste(col1,col2,collapse = '\n\n')  
+structPage = function(page) {
+  col1 = stri_extract_all_regex(page, ".*(   +)")[[1]]
+  col1 = paste(col1, collapse = '\n')
+  col2 = stri_extract_all_regex(page, "(   +).*\\n")[[1]]
+  col2 = paste(stri_replace_all_regex(col2, "(   +)", " "), collapse = '')
+  text = paste(col1, col2, collapse = '\n\n')
   return(text)
 }
 
 # Gets CNJ number
-getCNJOnReading = function(page){
+getCNJOnReading = function(page) {
   cnjs = c("First Document")
-  cnjs = c(cnjs, stri_extract_all_regex(page," \\d+[ ]?[-\\/][ ]?\\d{4}  +|\\d{1,7}-\\d{2}.\\d{4}.\\d[\\.]?\\d{2}.\\d{4}  +|\\d{14,20}  +| \\d+[ ]?[-\\/][ ]?\\d{4}\\n|\\d{1,7}-\\d{2}.\\d{4}.\\d[\\.]?\\d{2}.\\d{4}\\n|\\d{14,20}\\n",simplify = T))
-  cnjs = stri_replace_all_regex(cnjs,"\\n| ","")
+  cnjs = c(
+    cnjs,
+    stri_extract_all_regex(
+      page,
+      " \\d+[ ]?[-\\/][ ]?\\d{4}  +|\\d{1,7}-\\d{2}.\\d{4}.\\d[\\.]?\\d{2}.\\d{4}  +|\\d{14,20}  +| \\d+[ ]?[-\\/][ ]?\\d{4}\\n|\\d{1,7}-\\d{2}.\\d{4}.\\d[\\.]?\\d{2}.\\d{4}\\n|\\d{14,20}\\n",
+      simplify = T
+    )
+  )
+  cnjs = stri_replace_all_regex(cnjs,
+                                "\\n| ",
+                                "")
   return(cnjs[!is.na(cnjs)])
 }
 
 # Read justice diarie pdf
-readDocuments = function(file){
+readDocuments = function(file) {
   # Read as plain text
   doc = pdf_text(file)
   # Columns and pages in order
-  doc = unlist(lapply(doc,structPage))
+  doc = unlist(lapply(doc, structPage))
   # Paste all paragraphs
-  doc = paste(doc, collapse ="\n")
+  doc = paste(doc, collapse = "\n")
   # doc = tolower(doc)
   cnjs = getCNJOnReading(doc)
   # Split document by lawsuit
-  doc = strsplit(doc," \\d+[ ]?[-\\/][ ]?\\d{4}  +|\\d{1,7}-\\d{2}.\\d{4}.\\d[\\.]?\\d{2}.\\d{4}  +|\\d{14,20}  +| \\d+[ ]?[-\\/][ ]?\\d{4}\\n|\\d{1,7}-\\d{2}.\\d{4}.\\d[\\.]?\\d{2}.\\d{4}\\n|\\d{14,20}\\n", perl = T)[[1]]
+  doc = strsplit(
+    doc,
+    " \\d+[ ]?[-\\/][ ]?\\d{4}  +|\\d{1,7}-\\d{2}.\\d{4}.\\d[\\.]?\\d{2}.\\d{4}  +|\\d{14,20}  +| \\d+[ ]?[-\\/][ ]?\\d{4}\\n|\\d{1,7}-\\d{2}.\\d{4}.\\d[\\.]?\\d{2}.\\d{4}\\n|\\d{14,20}\\n",
+    perl = T
+  )[[1]]
   # Replace word used to split the documents
-  if(length(doc) > 1){
-    doc[2:length(doc)] = paste0("smtxdocumentseparationmarker ",cnjs[2:length(doc)],doc[2:length(doc)])
+  if (length(doc) > 1) {
+    doc[2:length(doc)] = paste0("smtxdocumentseparationmarker ",
+                                cnjs[2:length(doc)],
+                                doc[2:length(doc)])
   }
-  return(list(cnj=cnjs,doc=doc))
+  return(list(cnj = cnjs, doc = doc))
 }
 
-huntingLawsuits = function(fileName){
+huntingLawsuits = function(fileName) {
   ######################################################################################
   # Read Files
   ######################################################################################
-  file = paste0(path,fileName)
+  file = paste0(path, fileName)
   splitedDocs = readDocuments(file)
   cnjs = splitedDocs$cnj
-  digitsCNJ = nchar(stri_replace_all_regex(cnjs,"[[:punct:]]",""))
+  digitsCNJ = nchar(stri_replace_all_regex(cnjs,
+                                           "[[:punct:]]",
+                                           ""))
   docs = splitedDocs$doc
   doc.corpus = VCorpus(VectorSource(docs))
-
+  
   # remove older struct to optimize memory use
   rm(docs)
   invisible(gc())
   
   # Create metadata defining which document came from which diarie
-  doc.corpus = tm_map(doc.corpus, function(x){
+  doc.corpus = tm_map(doc.corpus, function(x) {
     meta(x, tag = "origin") <- fileName
     meta(x, tag = "totalChars") <- nchar(content(x))
     x
   })
   
   # Create metadata defining CNJ number and how many numerical digits it has
-  idx=0
-  doc.corpus = tm_map(doc.corpus, function(x){
+  idx = 0
+  doc.corpus = tm_map(doc.corpus, function(x) {
     idx <<- idx + 1
     meta(x, tag = "CNJ") <- cnjs[idx]
     meta(x, tag = "digitsCNJ") <- digitsCNJ[idx]
@@ -90,28 +110,57 @@ huntingLawsuits = function(fileName){
   # Cleaning Corpus
   ######################################################################################
   # Remove multiple blank space
-  doc.corpus = tm_map(doc.corpus, content_transformer(stri_replace_all_regex)," +"," ")
+  doc.corpus = tm_map(doc.corpus,
+                      content_transformer(stri_replace_all_regex),
+                      " +",
+                      " ")
   # Remove special characters
   doc.corpus = tm_map(doc.corpus, removePunctuation)
   # Transform string to lower case
   doc.corpus = tm_map(doc.corpus, content_transformer(tolower))
   
   #Filter Claro documents only
-  claro.corpus = tm_filter(doc.corpus, FUN = function(x) any(grepl("claro(\\n|\\s)+(s|sa|tv|celular|americel|pré)(\\s|$|\\n)", x)))
+  claro.corpus = tm_filter(
+    doc.corpus,
+    FUN = function(x)
+      any(
+        grepl(
+          "claro(\\n|\\s)+(s|sa|tv|celular|americel|pré)(\\s|$|\\n)",
+          x
+        )
+      )
+  )
   
   # remove older struct to optimize memory use
   rm(list = "doc.corpus")
   invisible(gc())
   
   # Saves claro's important metadata to a file
-  if(length(claro.corpus)>0){
-    claro = tidy(claro.corpus) %>% 
-      select(id, origin, totalChars, CNJ, digitsCNJ) %>% 
+  if (length(claro.corpus) > 0) {
+    claro = tidy(claro.corpus) %>%
+      select(id, origin, totalChars, CNJ, digitsCNJ) %>%
       as.data.table
     
-    claro[, formatedCNJ := ifelse(digitsCNJ>=12  & CNJ!="First Document",stri_pad_left(CNJ,20,'0'),CNJ)]
+    claro[, formatedCNJ := ifelse(
+      digitsCNJ >= 12  &
+        CNJ != "First Document",
+      gsub(
+        '(\\d{7})(\\d{2})(\\d{4})(\\d)(\\d{2})(\\d{4})',
+        '\\1-\\2\\.\\3\\.\\4\\.\\5\\.\\6',
+        stri_pad_left(stri_replace_all_regex(CNJ, "[[:punct:]]", ""), 20, '0')
+      ),
+      CNJ
+    )]
     
-    write.table(claro,outputFile,sep="\t",quote = T,row.names = F,col.names = F,append = T)
+    write.table(
+      claro,
+      outputFile,
+      sep = "\t",
+      quote = T,
+      row.names = F,
+      col.names = F,
+      append = T
+    )
   }
   invisible(gc())
 }
@@ -124,5 +173,7 @@ filesName = sort(filesName, decreasing = T)
 
 # Apply read function
 ncores = detectCores() - 1
-pbmclapply(filesName, huntingLawsuits, mc.cores = ncores, ignore.interactive = T)
-
+pbmclapply(filesName,
+           huntingLawsuits,
+           mc.cores = ncores,
+           ignore.interactive = T)
--- scripts/downloadDJ.R
+++ scripts/downloadDJ.R
@@ -2,17 +2,22 @@ library(rvest)
 library(parallel)
 library(pbmcapply)
 
-args = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly = TRUE)
 
 #################################
 # Check Arguments
 #################################
 if (length(args) != 3) {
-  stop("Error: Three arguments must be provided:\n\t1. Initial page number\n\t2. Final page number\n\t3. Output folder to save pdf documents", call.=FALSE)
+  stop(
+    "Error: Three arguments must be provided:\n\t1. Initial page number\n\t2. Final page number\n\t3. Output folder to save pdf documents",
+    call. = FALSE
+  )
 } else if (any(is.na(as.numeric(args[1:2])))) {
-  stop("Error: Arguments 1 and 2 must be numeric", call.=FALSE)
-} else if(as.numeric(args[2])<as.numeric(args[1])){
-  cat("Passed final page is smaller than initial page. The order was changed to attend the program requisites")
+  stop("Error: Arguments 1 and 2 must be numeric", call. = FALSE)
+} else if (as.numeric(args[2]) < as.numeric(args[1])) {
+  cat(
+    "Passed final page is smaller than initial page. The order was changed to attend the program requisites"
+  )
   aux = args[1]
   args[1] = args[2]
   args[2] = aux
@@ -26,33 +31,37 @@ finalPage = as.integer(args[2])
 outputFolder = args[3]
 
 #################################
-# Get all diaries editions number to download 
+# Get all diaries editions number to download
 #################################
-getEditions = function(page = '1'){
+getEditions = function(page = '1') {
   baseURL = "http://www.tjmt.jus.br/Djes?page="
   
-  editionInfo = read_html(paste0(baseURL,page)) %>% 
-    html_nodes("td.tamanho14.cinza.family") %>% 
-    html_nodes("b") %>% 
+  editionInfo = read_html(paste0(baseURL, page)) %>%
+    html_nodes("td.tamanho14.cinza.family") %>%
+    html_nodes("b") %>%
     html_text()
   
-  edition = editionInfo[seq(1,length(editionInfo),by=2)]
-  date = editionInfo[seq(2,length(editionInfo),by=2)]
-  year = sapply(strsplit(date,"/",fixed = T),function(x)x[3])
-  return(paste0(edition,'-',year))  
+  edition = editionInfo[seq(1, length(editionInfo), by = 2)]
+  date = editionInfo[seq(2, length(editionInfo), by = 2)]
+  year = sapply(strsplit(date, "/", fixed = T), function(x)
+    x[3])
+  return(paste0(edition, '-', year))
 }
 
 #################################
-# Download pdf into specified folder 
+# Download pdf into specified folder
 #################################
-downloadDiarie = function(edition, complementURL){
+downloadDiarie = function(edition, complementURL) {
   baseURL = "http://sistemadje.tjmt.jus.br/publicacoes/"
   tryCatch({
-    download.file(paste0(baseURL,edition,complementURL),paste0(outputFolder,edition,complementURL))
+    download.file(
+      paste0(baseURL, edition, complementURL),
+      paste0(outputFolder, edition, complementURL)
+    )
   },
-  error = function(cond){
+  error = function(cond) {
     return(NA)
-  }) 
+  })
 }
 
 #################################
@@ -61,29 +70,62 @@ downloadDiarie = function(edition, complementURL){
 ncores = detectCores() - 1
 
 # get editions numbers in all pages
-editions = pbmclapply(initialPage:finalPage, getEditions, mc.cores = ncores, ignore.interactive = T)
+editions = pbmclapply(
+  initialPage:finalPage,
+  getEditions,
+  mc.cores = ncores,
+  ignore.interactive = T
+)
 editionsVec = unlist(editions)
 
-# get when jornal changes page format  
-idxChangeDiarieName = which(grepl("9892",editionsVec,fixed = T))
-if(length(idxChangeDiarieName) == 0){
+# get when jornal changes page format
+idxChangeDiarieName = which(grepl("9892", editionsVec, fixed = T))
+if (length(idxChangeDiarieName) == 0) {
   idxChangeDiarieName = length(editionsVec)
 }
 # separates editions in the new and old format
-selectPattern = ifelse(seq_along(editionsVec) < idxChangeDiarieName,T,F)
+selectPattern = ifelse(seq_along(editionsVec) < idxChangeDiarieName, T, F)
 
 newerEditions = editionsVec[selectPattern]
 olderEditions = editionsVec[!selectPattern]
 
-if(length(newerEditions) > 0){
-  pbmclapply(newerEditions, downloadDiarie, complementURL = " C1 Tribunal de Justiça.pdf", mc.cores = ncores, ignore.interactive = T)
-  pbmclapply(newerEditions, downloadDiarie, complementURL = " C2 Comarcas - Entrância Especial.pdf", mc.cores = ncores, ignore.interactive = T)
-  pbmclapply(newerEditions, downloadDiarie, complementURL = " C7 Comarcas - 1ª 2ª e 3ª Entrância.pdf", mc.cores = ncores, ignore.interactive = T)
-  pbmclapply(newerEditions, downloadDiarie, complementURL = " C6 Foro Extrajudicial.pdf", mc.cores = ncores, ignore.interactive = T)
+if (length(newerEditions) > 0) {
+  pbmclapply(
+    newerEditions,
+    downloadDiarie,
+    complementURL = " C1 Tribunal de Justiça.pdf",
+    mc.cores = ncores,
+    ignore.interactive = T
+  )
+  pbmclapply(
+    newerEditions,
+    downloadDiarie,
+    complementURL = " C2 Comarcas - Entrância Especial.pdf",
+    mc.cores = ncores,
+    ignore.interactive = T
+  )
+  pbmclapply(
+    newerEditions,
+    downloadDiarie,
+    complementURL = " C7 Comarcas - 1ª 2ª e 3ª Entrância.pdf",
+    mc.cores = ncores,
+    ignore.interactive = T
+  )
+  pbmclapply(
+    newerEditions,
+    downloadDiarie,
+    complementURL = " C6 Foro Extrajudicial.pdf",
+    mc.cores = ncores,
+    ignore.interactive = T
+  )
 }
 
-if(length(olderEditions) > 0){
-  pbmclapply(olderEditions, downloadDiarie, complementURL = ".pdf", mc.cores = ncores, ignore.interactive = T)
+if (length(olderEditions) > 0) {
+  pbmclapply(
+    olderEditions,
+    downloadDiarie,
+    complementURL = ".pdf",
+    mc.cores = ncores,
+    ignore.interactive = T
+  )
 }
-
-
